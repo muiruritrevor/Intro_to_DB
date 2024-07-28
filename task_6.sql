9fc2b7d6a1cea37fb4c34e796c70d83be060eb0c
muiruritrevor <trevormuirurii@gmail.com>
2024-07-29 00:14:58 +0300

    Changed Insertion values

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,5 @@
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES 
-(2, 'Revy Martin', 'rm@sandtech.com', '31 bright  Ave.'),
-(3, 'Bright Mali', 'mali@sandtech.com', '31 bright  Ave.'),
-(4, 'Viva Trev', 'Viva@sandtech.com', '31 bright  Ave.');
+VALUES
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
