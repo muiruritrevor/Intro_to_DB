10883b7c0c4af0d094fd356308142270142162f4
muiruritrevor <trevormuirurii@gmail.com>
2024-07-29 00:07:21 +0300

    Add SQL script to populate Customers table with data

---
+++ task_6.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES 
+(2, 'Revy Martin', 'rm@sandtech.com', '31 bright  Ave.'),
+(3, 'Bright Mali', 'mali@sandtech.com', '31 bright  Ave.'),
+(4, 'Viva Trev', 'Viva@sandtech.com', '31 bright  Ave.');
